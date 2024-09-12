fa5ddc02692281fd9b2726e9bdabedfc26cd8029
aladores <angelo.ladores@outlook.com>
2024-09-11 21:11:49 -0400

    fix app.jsx import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home";
-import SimpleCoffee from "./pages/simpleCoffee/simpleCoffee";
+import SimpleCoffee from "./pages/simpleCoffee/SimpleCoffee";
 import Translate from "./pages/translate/Translate";
 import GitHubProfile from "./pages/githubProfile/GitHubProfile";
 import CountryTable from "./pages/worldRanks/CountryTable";
